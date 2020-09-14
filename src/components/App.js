0670f7d85b0eb7578019e66924a5f240194c323d
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2020-09-15 00:11:03 +0600

    Tweet Component

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 
+import LoadingBar from 'react-redux-loading'
+
 class App extends Component {
   componentDidMount() {
     this.props.dispatch(handleInitialData())
@@ -11,6 +13,7 @@ class App extends Component {
   render() {
     return (
       <div>
+        <LoadingBar />
         {this.props.loading === true
           ? null
           : <Dashboard />}
