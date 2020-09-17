ca8dc999a50298eb25ca79baec7e1d7f384e5280
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2020-09-17 12:33:50 +0600

    an already existing tweet needs to be modified if the new tweet is a response to another tweet

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import Dashboard from './Dashboard'
 import NewTweet from './NewTweet'
+import TweetPage from './TweetPage'
 
 import LoadingBar from 'react-redux-loading'
 
@@ -17,7 +18,7 @@ class App extends Component {
         <LoadingBar />
         {this.props.loading === true
           ? null
-          : <NewTweet />}
+          : <TweetPage match={{params: {id: '8xf0y6ziyjabvozdd253nd'}}} />}
 
       </div>
     )
